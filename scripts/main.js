380b73484a40c74d5ce413d1c149f38f94c7811a
Vanya-Dev <deynekaivan2020@gmail.com>
2026-04-11 22:24:32 +0300

    blog page

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,8 @@
 import Header from "./Header.js";
 import TabsCollection from "./Tabs.js";
 import VideoPlayerCollection from "./VideoPlayer.js";
-
+import ExpandableContentCollection from "./ExpandableContent.js";
 new Header();
 new TabsCollection();
 new VideoPlayerCollection();
+new ExpandableContentCollection();
